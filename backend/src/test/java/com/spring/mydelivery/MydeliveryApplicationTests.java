c4c2723fe0bb8a62847c1f747fbd6221568b82fb
sandoellytonj <sandoellytonj@gmail.com>
2021-01-19 21:23:26 -0300

    Created project

---
+++ backend/src/test/java/com/spring/mydelivery/MydeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.mydelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
