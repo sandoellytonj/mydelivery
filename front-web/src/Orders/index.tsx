d77b4722702f9ea2935f887f52d6c7b58db4e079
sandoellytonj <sandoellytonj@gmail.com>
2021-01-22 15:21:01 -0300

    api

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,10 +1,24 @@
+import ProductsList from './ProductsList'
 import StepsHeader from './StepsHeader'
 import './styles.css'
+import { useState, useEffect} from 'react'
+import { Product } from './types';
+import { fetchProducts } from '../api';
 
 function Orders() {
+
+    const [products, setProducts] = useState<Product[]>([]);
+    
+    useEffect(() => {   
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products={products}/>
         </div>
     )
 }
